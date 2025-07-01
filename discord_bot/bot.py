19aef5ae37395ca6b3bf35bc489d02e78cd471a2
Mark Vos <ranger.ahilles@gmail.com>
2025-07-01 15:21:03 +0300

    added github actions

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -35,6 +35,8 @@ class Mybot:
 
             if message.content.startswith('$games'):
                 lambda: asyncio.create_task(send_avalible_games(message=message))
+            if message.content.startswith('$goida'):
+                await message.channel.send('Hello there')
 
         client.run(TOKEN)
         
