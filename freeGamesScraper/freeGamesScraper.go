1a808cd190a4fa7fb12b567d0397e37c193300df
TechMarcus <ranger.ahilles@gmail.com>
2025-12-06 17:57:36 +0200

    fixes

--- freeGamesScraper/freeGamesScraper.go
+++ freeGamesScraper/freeGamesScraper.go
@@ -123,7 +123,7 @@ func GameInfoToJson(games []GameInfo, jsonfile string) error {
 	if error != nil {
 		return error
 	}
-	file, err := os.OpenFile(jsonfile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	file, err := os.OpenFile(jsonfile, os.O_RDWR|os.O_TRUNC, 0755)
 	if err != nil {
 		return err
 	}
